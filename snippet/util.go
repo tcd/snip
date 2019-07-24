99308060f662a1358cb53d27bee5b0ffa0955035
Clay Dunston <dunstontc@gmail.com>
2019-07-24 13:28:03 -0500

    Update `snippet/util`
    Changes:
    - Remove unused File structs
    - Rename `getBytes` to `bytesFromFile`
    - Add `getGroups` function

--- snippet/util.go
+++ snippet/util.go
@@ -3,25 +3,11 @@ package snippet
 import (
 	"io/ioutil"
 	"log"
+	"regexp"
 )
 
-// File models a file and comes with some handy goodness.
-type File struct {
-	Path     string
-	Contents []byte
-}
-
-// NewFile news up a new File.
-func NewFile(path string) *File {
-	file := File{
-		Path:     path,
-		Contents: getBytes(path),
-	}
-	return &file
-}
-
 // Gets a slice of bytes from a file.
-func getBytes(file string) []byte {
+func bytesFromFile(file string) []byte {
 	bytes, err := ioutil.ReadFile(file) // TODO: use `func ReadAll(r io.Reader) ([]byte, error)``
 	if err != nil {
 		log.Fatalf("Error reading file '%s': %s", file, err.Error())
@@ -30,7 +16,24 @@ func getBytes(file string) []byte {
 	return bytes
 }
 
-// Return a File's contents as a string.
-func (f *File) String() string {
-	return string(f.Contents)
+// getGroups parses testString with the given regular expression
+// and returns the group values defined in the expression.
+//
+// Thanks eluleci (https://stackoverflow.com/a/39635221/7687024)
+//
+func getGroups(regEx, testString string) (groupsMap map[string]string) {
+
+	compiledRegEx := regexp.MustCompile(regEx)
+
+	match := compiledRegEx.FindStringSubmatch(testString)
+
+	groupsMap = make(map[string]string)
+
+	for i, name := range compiledRegEx.SubexpNames() {
+		if i > 0 && i <= len(match) {
+			groupsMap[name] = match[i]
+		}
+	}
+
+	return groupsMap
 }
