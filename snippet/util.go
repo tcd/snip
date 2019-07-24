cc1fc0e3614c420aaa4cd23941e81abe6e832e93
Clay Dunston <dunstontc@gmail.com>
2019-07-24 15:23:14 -0500

    Update utility functions for package snippet
    - remove `bytesFromFile()`
    - add `compareSnippets()`

--- snippet/util.go
+++ snippet/util.go
@@ -1,20 +1,9 @@
 package snippet
 
 import (
-	"io/ioutil"
-	"log"
+	"errors"
 	"regexp"
 )
-
-// Gets a slice of bytes from a file.
-func bytesFromFile(file string) []byte {
-	bytes, err := ioutil.ReadFile(file) // TODO: use `func ReadAll(r io.Reader) ([]byte, error)``
-	if err != nil {
-		log.Fatalf("Error reading file '%s': %s", file, err.Error())
-		return []byte{}
-	}
-	return bytes
-}
 
 // getGroups parses testString with the given regular expression
 // and returns the group values defined in the expression.
@@ -36,4 +25,25 @@ func getGroups(regEx, testString string) (groupsMap map[string]string) {
 	}
 
 	return groupsMap
+}
+
+func compareSnippets(have Snippet, want Snippet) error {
+	if have.Name != want.Name {
+		return errors.New("Name mismatch:\nhave = " + have.Name + "\nwant = " + want.Name)
+	}
+	if have.Trigger != want.Trigger {
+		return errors.New("Trigger mismatch:\nhave = " + have.Trigger + "\nwant = " + want.Trigger)
+	}
+	if have.Rules != want.Rules {
+		return errors.New("Rules mismatch:\nhave = " + have.Rules + "\nwant = " + want.Rules)
+	}
+	if have.Description != want.Description {
+		return errors.New("Description mismatch:\nhave = " + have.Description + "\nwant = " + want.Description)
+	}
+	for i := range have.Body {
+		if have.Body[i] != want.Body[i] {
+			return errors.New("Body mismatch:\nhave = " + have.Body[i] + "\nwant = " + want.Body[i])
+		}
+	}
+	return nil
 }
