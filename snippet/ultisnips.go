07a5a41f2838023c4e7abc69d46f7bcfd0b52e57
Clay Dunston <dunstontc@gmail.com>
2019-07-24 13:32:06 -0500

    Add/Update Ultisnips snippet parsing

--- snippet/ultisnips.go
+++ snippet/ultisnips.go
@@ -2,25 +2,31 @@
 package snippet
 
 import (
-	"io/ioutil"
-	"log"
 	"regexp"
+	"strings"
 )
 
-// ParseFile returns an array of snippets in a file at the given path.
-func ParseFile(path string) []string {
-	bytes := getBytes(path)
-	re := regexp.MustCompile(`(?msU:((^snippet.+$\n)(.+)(?:endsnippet)))`)
-	matches := re.FindAllString(string(bytes), -1) // TODO: Find out if FindAll() is faster than FindAllString()
-	return matches
+// ParseUltisnipsSnippet parses a raw string UltiSnips snippet.
+func ParseUltisnipsSnippet(s string) Snippet {
+
+	lines := strings.Split(s, "\n")
+
+	matches := getGroups(`^snippet\s+(?P<trigger>.?\w+.?)\s(?P<description>".+")\s(?P<options>[biemrstwA]+)?$`, lines[0])
+	body := lines[:len(lines)-2]
+
+	return Snippet{
+		Name:        "",
+		Trigger:     matches["trigger"],
+		Rules:       matches["rules"],
+		Body:        body,
+		Description: matches["description"],
+	}
 }
 
-// Gets a slice of bytes from a file; return value will be empty if an error occurs.
-func getBytes(file string) []byte {
-	bytes, err := ioutil.ReadFile(file) // TODO: use `func ReadAll(r io.Reader) ([]byte, error)``
-	if err != nil {
-		log.Fatalf("Demonic Invasion In Progress: %s", err.Error())
-		return []byte{}
-	}
-	return bytes
+// ParseUltisnipsFile returns an array of strings containing all snippets in a file at the given path.
+func ParseUltisnipsFile(path string) []string {
+	bytes := bytesFromFile(path)
+	re := regexp.MustCompile(`(?msU:((^snippet.+$\n)(.+)(?:endsnippet)))`)
+	matches := re.FindAllString(string(bytes), -1)
+	return matches
 }
