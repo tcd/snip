069ae1a218defbe3a0a0e2784422b367260c1e74
Clay Dunston <claydunston@gmail.com>
2019-07-24 23:36:42 -0500

    Update Snippet's String() function

--- snippet/snippet.go
+++ snippet/snippet.go
@@ -1,23 +1,34 @@
 // Package snippet provides snippet models.
 package snippet
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Snippet models a platform independent snippet.
 type Snippet struct {
 	Name        string
 	Trigger     string
+	Description string
 	Rules       string
 	Body        []string
-	Description string
 }
 
 func (s Snippet) String() string {
 	return fmt.Sprint(
-		"Name: ", s.Name, "\n",
-		"Trigger: ", s.Trigger, "\n",
-		"Rules: ", s.Rules, "\n",
-		"Body: ", s.Body, "\n",
-		"Description: ", s.Description,
+		"Name:        ", s.Name, "\n",
+		"Trigger:     ", s.Trigger, "\n",
+		"Description: ", s.Description, "\n",
+		"Rules:       ", s.Rules, "\n",
+		"Body:        ", "\n", bodyLoop(s.Body),
 	)
+}
+
+func bodyLoop(body []string) string {
+	var sb strings.Builder
+	for _, str := range body {
+		sb.WriteString(str + "\n")
+	}
+	return sb.String()
 }
