57409915cc2d290fa9d2cc5dd73a40eecba8d998
Clay Dunston <claydunston@gmail.com>
2019-07-25 20:59:34 -0500

    Update snip.Snippet
    - Add Scope field to snip.Snippet
    - Improve Snippet.String() (though it could probably be cleaner)

--- snippet/snippet.go
+++ snippet/snippet.go
@@ -1,10 +1,7 @@
 // Package snippet provides snippet models.
 package snippet
 
-import (
-	"fmt"
-	"strings"
-)
+import "strings"
 
 // Snippet models a platform independent snippet.
 type Snippet struct {
@@ -12,23 +9,38 @@ type Snippet struct {
 	Trigger     string
 	Description string
 	Rules       string
+	Scope       string
 	Body        []string
 }
 
 func (s Snippet) String() string {
-	return fmt.Sprint(
-		"Name:        ", s.Name, "\n",
-		"Trigger:     ", s.Trigger, "\n",
-		"Description: ", s.Description, "\n",
-		"Rules:       ", s.Rules, "\n",
-		"Body:        ", "\n", bodyLoop(s.Body),
-	)
-}
-
-func bodyLoop(body []string) string {
 	var sb strings.Builder
-	for _, str := range body {
-		sb.WriteString(str + "\n")
+
+	if s.Name != "" {
+		sb.WriteString("Name:        " + s.Name + "\n")
 	}
+	if s.Trigger != "" {
+		sb.WriteString("Trigger:     " + s.Trigger + "\n")
+	}
+	if s.Description != "" {
+		sb.WriteString("Description: " + s.Description + "\n")
+	}
+	if s.Scope != "" {
+		sb.WriteString("Scope:       " + s.Scope + "\n")
+	}
+	if s.Rules != "" {
+		sb.WriteString("Rules:       " + s.Rules + "\n")
+	}
+
+	sb.WriteString("Body:\n")
+	length := len(s.Body) - 1
+	for i, str := range s.Body {
+		if i == length {
+			sb.WriteString(str)
+		} else {
+			sb.WriteString(str + "\n")
+		}
+	}
+
 	return sb.String()
 }
