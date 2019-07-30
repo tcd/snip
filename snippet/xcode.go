8935a580452cc1cb167f455f0732c6e6adad5de0
Clay Dunston <claydunston@gmail.com>
2019-07-30 15:44:13 -0500

    Add code in xcode.go

--- snippet/xcode.go
+++ snippet/xcode.go
@@ -1,12 +1,46 @@
 package snippet
 
+import (
+	"io/ioutil"
+	"strings"
+
+	"howett.net/plist"
+)
+
 type xcodeSnippet struct {
 	Trigger          string   `plist:"IDECodeSnippetCompletionPrefix"`
 	Title            string   `plist:"IDECodeSnippetTitle"`
-	Scopes           string   `plist:"IDECodeSnippetLanguage"`
+	Scope            string   `plist:"IDECodeSnippetLanguage"`
+	Summary          string   `plist:"IDECodeSnippetSummary"`
 	Contents         string   `plist:"IDECodeSnippetContents"`
 	CompletionScopes []string `plist:"IDECodeSnippetCompletionScopes"`
 	UUID             string   `plist:"IDECodeSnippetIdentifier"`
 	UserSnippet      bool     `plist:"IDECodeSnippetUserSnippet"`
 	SnippetVersion   int      `plist:"IDECodeSnippetVersion"`
+}
+
+// ParseXcodeFile Parses a plist file containing an Xcode snippet.
+func ParseXcodeFile(path string) (Snippet, error) {
+	var s Snippet
+	var xs xcodeSnippet
+
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return s, err
+	}
+
+	_, err = plist.Unmarshal(bytes, &xs)
+	if err != nil {
+		return s, err
+	}
+
+	return xcodeSnippetToSnippet(xs), nil
+}
+
+func xcodeSnippetToSnippet(s xcodeSnippet) Snippet {
+	return Snippet{
+		Trigger:     strings.TrimSpace(s.Trigger),
+		Body:        strings.Split(s.Contents, "\n"),
+		Description: strings.TrimSpace(s.Title),
+	}
 }
