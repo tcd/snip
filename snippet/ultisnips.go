65efe8babfbcd2a2aab5b58a1c5d1b479e8797c4
Clay Dunston <claydunston@gmail.com>
2019-07-30 13:20:44 -0500

    Add logic for identifying Ultisnips code interpolation
    Also adds experimental Ultisnips method to Snippet

--- snippet/ultisnips.go
+++ snippet/ultisnips.go
@@ -7,8 +7,30 @@ import (
 	"strings"
 )
 
+// Ultisnips returns a Snippet in Ultisnips format.
+func (s Snippet) Ultisnips() string {
+
+	var sb strings.Builder
+
+	sb.WriteString("snippet")
+	sb.WriteString(" " + s.Trigger)
+	if s.Description != "" {
+		sb.WriteString(" \"" + s.Description + "\"")
+	}
+	if s.Rules != "" {
+		sb.WriteString(" " + s.Rules)
+	}
+	sb.WriteString("\n")
+	sb.WriteString(strings.Join(s.Body, "\n"))
+	sb.WriteString("\n")
+	sb.WriteString("endsnippet")
+
+	return sb.String()
+}
+
 // ParseUltisnipsFile returns an array of strings containing all snippets in a file at the given path.
 func ParseUltisnipsFile(path string) ([]Snippet, error) {
+
 	var snippets []Snippet
 
 	bytes, err := ioutil.ReadFile(path)
@@ -28,8 +50,27 @@ func ParseUltisnipsFile(path string) ([]Snippet, error) {
 func ParseUltisnipsSnippet(s string) Snippet {
 
 	lines := strings.Split(s, "\n")
+	header := lines[0]
+	// body := strings.Join(lines[1:len(lines)-1], "\n")
 
-	matches := getGroups(`^snippet\s+(?P<trigger>.?\w+.?)\s(?P<description>".+")\s(?P<options>[biemrstwA]+$)?`, lines[0])
+	// options := regexp.MustCompile(`(?:\s+)([biemrstwA]+$)`).FindAllString(header, -1)
+	// if len(options) == 1 {
+	// 	if strings.Contains(options[0], "r") {
+	// 		// handle delims for trigger
+	// 	}
+	// }
+
+	// Python Interpolation: `\x60\!p[^\x60]+\x60`
+	// Any interpolationL `\x60[^\x60]+\x60
+	// interpolations := regexp.MustCompile("`[^`]+`").FindAllString(body, -1)
+	// if len(interpolations) > 0 {
+	// 	if strings.Contains(interpolations[0], "r") {
+	// 		// handle delims for trigger
+	// 		fmt.Println(interpolations)
+	// 	}
+	// }
+
+	matches := getGroups(`^snippet\s+(?P<trigger>.?\w+.?)\s(?P<description>".+")\s(?P<options>[biemrstwA]+$)?`, header)
 
 	return Snippet{
 		Name:        "",
