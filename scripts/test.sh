6109b525f0ab58d1f9e508e63984e67d403f9c70
Clay Dunston <claydunston@gmail.com>
2019-07-29 21:21:26 -0500

    Run tests once

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
-go test -v ./... -count=10 | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+go test -v ./... -count=1 | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
 
